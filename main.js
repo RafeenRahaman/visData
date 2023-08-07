16e8354f73bc432328caa2146e7b7b7d3c25ebf6
Rafeen Rahaman <rafeenziarahaman.com>
2023-08-07 11:44:38 -0400

    commit

---
+++ main.js
@@ -0,0 +1,161 @@
+let button0 = document.getElementById('person0')
+let button1 = document.getElementById('person1')
+let button2 = document.getElementById('person2')
+let button3 = document.getElementById('person3')
+let button4 = document.getElementById('person4')
+let button5 = document.getElementById('person5')
+let button6 = document.getElementById('person6')
+let button7 = document.getElementById('person7')
+let button8 = document.getElementById('person8')
+let button9 = document.getElementById('person9')
+
+function function0() {
+    let profile = document.getElementById('profile')
+    let name = document.getElementById('name')
+    let email = document.getElementById('email')
+    let address = document.getElementById('address')
+    let phone = document.getElementById('phone')
+    let password = document.getElementById('password')
+
+    name.innerText = "Name: " + data.results[0].name.first + " " + data.results[0].name.last
+    email.innerText = "Email: " + data.results[0].email
+    address.innerText = "Address: " + data.results[0].location.street.number + " " + data.results[0].location.street.name
+    phone.innerText = "Phone: " + data.results[0].phone
+    password.innerText = "Password: " + data.results[0].login.password
+}
+function function1() {
+    let profile = document.getElementById('profile')
+    let name = document.getElementById('name')
+    let email = document.getElementById('email')
+    let address = document.getElementById('address')
+    let phone = document.getElementById('phone')
+    let password = document.getElementById('password')
+
+    name.innerText = "Name: " + data.results[1].name.first + " " + data.results[1].name.last
+    email.innerText = "Email: " + data.results[1].email
+    address.innerText = "Address: " + data.results[1].location.street.number + " " + data.results[1].location.street.name
+    phone.innerText = "Phone: " + data.results[1].phone
+    password.innerText = "Password: " + data.results[1].login.password
+}
+function function2() {
+    let profile = document.getElementById('profile')
+    let name = document.getElementById('name')
+    let email = document.getElementById('email')
+    let address = document.getElementById('address')
+    let phone = document.getElementById('phone')
+    let password = document.getElementById('password')
+
+    name.innerText = "Name: " + data.results[2].name.first + " " + data.results[2].name.last
+    email.innerText = "Email: " + data.results[2].email
+    address.innerText = "Address: " + data.results[2].location.street.number + " " + data.results[2].location.street.name
+    phone.innerText = "Phone: " + data.results[2].phone
+    password.innerText = "Password: " + data.results[2].login.password
+}
+function function3() {
+    let profile = document.getElementById('profile')
+    let name = document.getElementById('name')
+    let email = document.getElementById('email')
+    let address = document.getElementById('address')
+    let phone = document.getElementById('phone')
+    let password = document.getElementById('password')
+
+    name.innerText = "Name: " + data.results[3].name.first + " " + data.results[3].name.last
+    email.innerText = "Email: " + data.results[3].email
+    address.innerText = "Address: " + data.results[3].location.street.number + " " + data.results[3].location.street.name
+    phone.innerText = "Phone: " + data.results[3].phone
+    password.innerText = "Password: " + data.results[3].login.password
+}
+function function4() {
+    let profile = document.getElementById('profile')
+    let name = document.getElementById('name')
+    let email = document.getElementById('email')
+    let address = document.getElementById('address')
+    let phone = document.getElementById('phone')
+    let password = document.getElementById('password')
+
+    name.innerText = "Name: " + data.results[4].name.first + " " + data.results[4].name.last
+    email.innerText = "Email: " + data.results[4].email
+    address.innerText = "Address: " + data.results[4].location.street.number + " " + data.results[4].location.street.name
+    phone.innerText = "Phone: " + data.results[4].phone
+    password.innerText = "Password: " + data.results[4].login.password
+}
+function function5() {
+    let profile = document.getElementById('profile')
+    let name = document.getElementById('name')
+    let email = document.getElementById('email')
+    let address = document.getElementById('address')
+    let phone = document.getElementById('phone')
+    let password = document.getElementById('password')
+
+    name.innerText = "Name: " + data.results[5].name.first + " " + data.results[5].name.last
+    email.innerText = "Email: " + data.results[5].email
+    address.innerText = "Address: " + data.results[5].location.street.number + " " + data.results[5].location.street.name
+    phone.innerText = "Phone: " + data.results[5].phone
+    password.innerText = "Password: " + data.results[5].login.password
+}
+function function6() {
+    let profile = document.getElementById('profile')
+    let name = document.getElementById('name')
+    let email = document.getElementById('email')
+    let address = document.getElementById('address')
+    let phone = document.getElementById('phone')
+    let password = document.getElementById('password')
+
+    name.innerText = "Name: " + data.results[6].name.first + " " + data.results[6].name.last
+    email.innerText = "Email: " + data.results[6].email
+    address.innerText = "Address: " + data.results[6].location.street.number + " " + data.results[6].location.street.name
+    phone.innerText = "Phone: " + data.results[6].phone
+    password.innerText = "Password: " + data.results[6].login.password
+}
+function function7() {
+    let profile = document.getElementById('profile')
+    let name = document.getElementById('name')
+    let email = document.getElementById('email')
+    let address = document.getElementById('address')
+    let phone = document.getElementById('phone')
+    let password = document.getElementById('password')
+
+    name.innerText = "Name: " + data.results[7].name.first + " " + data.results[7].name.last
+    email.innerText = "Email: " + data.results[7].email
+    address.innerText = "Address: " + data.results[7].location.street.number + " " + data.results[7].location.street.name
+    phone.innerText = "Phone: " + data.results[7].phone
+    password.innerText = "Password: " + data.results[7].login.password
+}
+function function8() {
+    let profile = document.getElementById('profile')
+    let name = document.getElementById('name')
+    let email = document.getElementById('email')
+    let address = document.getElementById('address')
+    let phone = document.getElementById('phone')
+    let password = document.getElementById('password')
+
+    name.innerText = "Name: " + data.results[8].name.first + " " + data.results[8].name.last
+    email.innerText = "Email: " + data.results[8].email
+    address.innerText = "Address: " + data.results[8].location.street.number + " " + data.results[8].location.street.name
+    phone.innerText = "Phone: " + data.results[8].phone
+    password.innerText = "Password: " + data.results[8].login.password
+}
+function function9() {
+    let profile = document.getElementById('profile')
+    let name = document.getElementById('name')
+    let email = document.getElementById('email')
+    let address = document.getElementById('address')
+    let phone = document.getElementById('phone')
+    let password = document.getElementById('password')
+
+    name.innerText = "Name: " + data.results[9].name.first + " " + data.results[9].name.last
+    email.innerText = "Email: " + data.results[9].email
+    address.innerText = "Address: " + data.results[9].location.street.number + " " + data.results[9].location.street.name
+    phone.innerText = "Phone: " + data.results[9].phone
+    password.innerText = "Password: " + data.results[9].login.password
+}
+button0.addEventListener("click", function0)
+button1.addEventListener("click", function1)
+button2.addEventListener("click", function2)
+button3.addEventListener("click", function3)
+button4.addEventListener("click", function4)
+button5.addEventListener("click", function5)
+button6.addEventListener("click", function6)
+button7.addEventListener("click", function7)
+button8.addEventListener("click", function8)
+button9.addEventListener("click", function9)
